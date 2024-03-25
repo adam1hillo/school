aa1013a46281b99087e1b6e9a86fbf3d0ba3e836
Adam Hillo <adam1hillo@gmail.com>
2024-03-25 14:49:12 +0100

    LeerlingRepository

--- src/main/java/be/vdab/school/leerlingen/LeerlingRepository.java
+++ src/main/java/be/vdab/school/leerlingen/LeerlingRepository.java
@@ -2,9 +2,23 @@ package be.vdab.school.leerlingen;
 
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 @Component
 public class LeerlingRepository {
-    public LeerlingRepository() {
-        System.out.println("LeerlingRepository constructor");
+
+    public List<Leerling> findAll() {
+        try (var stream = Files.lines(Path.of("/data/leerlingen.csv"))) {
+            return stream
+                    .map(regel -> regel.split(","))
+                    .map(regelOnderdelen ->
+                            new Leerling(Long.parseLong(regelOnderdelen[0]), regelOnderdelen[1], regelOnderdelen[2]))
+                    .toList();
+        } catch (IOException | ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+            throw new IllegalArgumentException("Leerlingenbestand bevat verkeerde data", ex);
+        }
     }
 }
